e34a2fef32e5a65120fa2b1fd3d36dbdc7b2cb79
ElliotWride <elliotwride@gmail.com>
2023-03-09 14:05:27 +0000

    very broken

--- app/src/main/java/com/example/recview/PlayerActivity.java
+++ app/src/main/java/com/example/recview/PlayerActivity.java
@@ -2,37 +2,91 @@ package com.example.recview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.media.Image;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.media.MediaPlayer;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.testing.json.MockJsonFactory;
 import com.google.api.services.customsearch.v1.CustomSearchAPI;
-import com.google.api.services.customsearch.v1.model.Result;
+import com.google.api.services.customsearch.v1.CustomSearchAPIRequestInitializer;
 import com.google.api.services.customsearch.v1.model.Search;
 
 
+import java.io.InputStream;
 import java.io.Serializable;
+import java.net.URL;
 import java.util.List;
 
 
+
+
 public class PlayerActivity extends AppCompatActivity implements Serializable {
+
 
     // Instantiating the MediaPlayer class
     MediaPlayer music;
     String currentSong;
     String oldSong;
+    String albumURL = null;
+    //SharedPreferences pref = this.getSharedPreferences("com.example.recview", Context.MODE_PRIVATE);
 
+    class AlbumCoverFetcher extends AsyncTask<String, Void, String> {
+        private Context parent;
+
+        public AlbumCoverFetcher(Context context){
+            parent = context;
+        }
+
+        protected String doInBackground(String... strings) {
+            com.google.api.services.customsearch.v1.model.Result result = null;
+            Search results = null;
+            try {
+                CustomSearchAPI cs = new CustomSearchAPI.Builder(GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(), null).setApplicationName("recview")
+                        .setGoogleClientRequestInitializer(new CustomSearchAPIRequestInitializer("AIzaSyBp6YlE_Z7O95oy20cL65VPFjOB-6zPmdk"))
+                        .build();
+                CustomSearchAPI.Cse.List list = cs.cse().list().setQ("orange").setCx("f6276d7a5ac5c43c5");
+                System.out.println(list);
+                //Toast.makeText(this, list.toString(), Toast.LENGTH_SHORT).show();
+                results = list.execute();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println(results.size());
+            //SharedPreferences prefs = parent.getSharedPreferences("com.example.recview", Context.MODE_PRIVATE);
+            String urlString = results.getItems().get(0).getImage().getThumbnailLink();
+            try {
+                ImageView iv = (ImageView) findViewById(R.id.imageView);
+                InputStream is = (InputStream) new URL(urlString).getContent();
+                Drawable d = Drawable.createFromStream(is, "src name");
+                iv.setImageDrawable(d);
+            } catch (Exception e) {
+
+            }
+            return "test";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+        }
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
         Bundle bundle = getIntent().getExtras();
@@ -46,51 +100,16 @@ public class PlayerActivity extends AppCompatActivity implements Serializable {
         //Toast.makeText(this, currentSong, Toast.LENGTH_SHORT).show();
         MainActivity.audioManager.setContext(this);
         Bitmap pic = MainActivity.audioManager.getEmbeddedPicture();
-        ImageView iv = (ImageView)findViewById(R.id.imageView);
-        if (pic!=null){
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        //if (pic!=null){
+        if (false) {
             iv.setImageBitmap(pic);
-        }else {
-            Result result = search("album");
-            result.get();
-
+        } else {
+            AlbumCoverFetcher albumCoverFetcher = new AlbumCoverFetcher(this);
+            albumCoverFetcher.execute();
         }
     }
 
-    private com.google.api.services.customsearch.v1.model.Result search(String keyword){
-        CustomSearchAPI customsearch= null;
-
-
-        try {
-            customsearch = new CustomSearchAPI(new NetHttpTransport(), new MockJsonFactory(), new HttpRequestInitializer() {
-                public void initialize(HttpRequest httpRequest) {
-                    try {
-                        // set connect and read timeouts
-                        int HTTP_REQUEST_TIMEOUT = 1000;
-                        httpRequest.setConnectTimeout(HTTP_REQUEST_TIMEOUT);
-                        httpRequest.setReadTimeout(HTTP_REQUEST_TIMEOUT);
-
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        List<com.google.api.services.customsearch.v1.model.Result> resultList=null;
-        com.google.api.services.customsearch.v1.model.Result result = null;
-        try {
-            CustomSearchAPI.Cse.List list = customsearch.cse().list(keyword);
-            list.setKey("AIzaSyBp6YlE_Z7O95oy20cL65VPFjOB-6zPmdk");
-            list.setCx("f6276d7a5ac5c43c5");
-            Search results=list.execute();
-            result=results.getItems().get(0);
-        }
-        catch (  Exception e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
 
     // Playing the music
     public void musicplay(View v)
@@ -111,7 +130,7 @@ public class PlayerActivity extends AppCompatActivity implements Serializable {
         MainActivity.audioManager.stopSong();
     }
 
-
-
-
+    public void setAlbumURL(String albumURL) {
+        this.albumURL = albumURL;
+    }
 }
